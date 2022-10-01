f697c4c9f5144bfac1c30a248aacd7e978a3ccc6
alex-sasha-ward <amwtranslations@gmail.com>
2022-10-01 12:46:56 +0100

    Model updates

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from nturl2path import url2pathname
+from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.urls import reverse
 from datetime import date
@@ -6,13 +7,17 @@ from datetime import date
 # Create your models here.
 
 # class User(models.Model)
-# class Destination(models.Model):
-#     name = models.CharField(max_length=50)
-#     color = models.CharField(max_length=20)
+class Destination(models.Model):
+    name = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    currency = models.CharField(max_length=20)
+    description = models.CharField(max_length=200)
+    images = image = models.ImageField(upload_to ='main_app/static/uploads/', default="")
+    keywords = models.CharField(max_)
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
 
-#     def get_absolute_url(self):
-#         return reverse('destinations_detail', kwargs={'pk': self.id})
+    def get_absolute_url(self):
+        return reverse('destinations_detail', kwargs={'pk': self.id})
 
