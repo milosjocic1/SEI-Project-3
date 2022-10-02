6583dea04872f7b349bb2b1db8ae8b5d2c86b11b
alex-sasha-ward <amwtranslations@gmail.com>
2022-10-02 14:58:47 +0100

    Destination model created

--- main_app/models.py
+++ main_app/models.py
@@ -8,16 +8,15 @@ from datetime import date
 
 # class User(models.Model)
 class Destination(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=50)
     country = models.CharField(max_length=50)
-    currency = models.CharField(max_length=20)
-    description = models.CharField(max_length=200)
-    images = image = models.ImageField(upload_to ='main_app/static/uploads/', default="")
-    keywords = models.CharField(max_)
+    currency = models.CharField(max_length=100)
+    image = models.CharField(max_length=500)
+    keywords = models.CharField(max_length=20)
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('destinations_detail', kwargs={'pk': self.id})
-
