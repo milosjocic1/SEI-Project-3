9657a19cf1f1ca4d79bb41ff5fa6de1a66c556e4
alex-sasha-ward <amwtranslations@gmail.com>
2022-10-03 15:59:01 +0100

    Login and logout and sign-up added

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('destinations/<int:destination_id>', views.destinations_detail, name = 'detail'),
     path('destinations/<int:destination_id><int:user_id>/add_review', views.add_review, name='add_review'),
     # Quiz
-    path('quiz/', views.quiz, name='quiz')
+    path('quiz/', views.quiz, name='quiz'),
+    path('accounts/signup/', views.signup, name='signup')
 ]
