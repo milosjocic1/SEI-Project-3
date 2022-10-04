d5a7cef4e1760f24bc278778279fe6cadfcc817e
alex-sasha-ward <amwtranslations@gmail.com>
2022-10-04 12:49:53 +0100

    Adding in Profile functionality

--- main_app/models.py
+++ main_app/models.py
@@ -50,5 +50,13 @@ class Rating(forms.Form):
     def __str__(self):
         return self.ratings
     
-    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    avatar = models.ImageField(default='default.jpg', upload_to='profile_images')
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.user.username
+
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Destination, Rating, Review
-from .forms import ReviewForm
+from .forms import ReviewForm, UpdateUserForm, UpdateProfileForm
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -77,7 +78,20 @@ def add_rating(request, destination_id, user_id):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        profile_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your profile is updated successfully')
+            return redirect(to='users-profile')
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        profile_form = UpdateProfileForm(instance=request.user.profile)
+
+    return render(request, 'users/profile.html', {'user_form': user_form, 'profile_form': profile_form})
 
 def signup(request):
     error_message = ""
