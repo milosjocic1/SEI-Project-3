1b117576adef0f6a393c3fcb945b615d481d950a
alex-sasha-ward <amwtranslations@gmail.com>
2022-10-05 10:04:12 +0100

    Changes

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,12 +10,8 @@ urlpatterns = [
     # Quiz
     path('quiz/', views.quiz, name='quiz'),
     path('accounts/signup/', views.signup, name='signup'),
-<<<<<<< HEAD
     path('users/profile/', views.profile, name='users_profile'),
-    path('users/profile_update/', views.profile, name='users_profile_update')
-=======
-    path('users/profile/', views.profile, name='users-profile'),
+    path('users/profile_update/', views.profile, name='users_profile_update'),
     path('search/', views.search, name="search")
->>>>>>> dcdd4992e09c4a6e14f6eb0ac6e34781d0753d70
 ]
  
