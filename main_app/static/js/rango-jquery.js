db05516789591907ccbe339ae5fe0bd175adef2d
alex-sasha-ward <amwtranslations@gmail.com>
2022-10-06 15:50:07 +0100

    Update

--- main_app/static/js/rango-jquery.js
+++ main_app/static/js/rango-jquery.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    
+$('#quiz-button').hide()    
 let questionsToAsk = ["Which is your happy place?", "Do you like it hot - or not?", "Do you love romance or long for adventure?", "Are you a culture vulture or do you love to explore?"];
 let answers = ["The countryside", "The city", "Sandy beaches", "Snowy landscapes", "Going on safari", "Croissants in Paris", "Bring me the horizon", "Give me history"]
 let keywords = ["nature", "city", "summer", "winter", "adventure", "romantic", "exotic", "culture"];
@@ -11,9 +11,10 @@ let question = $(".quiz-question");
 let answerOne = $('#answerOne');
 let answerTwo = $('#answerTwo');
 question.text(questionsToAsk[i]);
-answerOne.text(answers[i]);
-answerTwo.text(answers[i + 1]);
+answerOne.text(answers[j]);
+answerTwo.text(answers[k]);
 answerOne.on("click", function() {
+    if (i < 3 ) {
     question.fadeOut(1000, function() {
         question.text(questionsToAsk[i]).fadeIn(1000);
     });
@@ -28,14 +29,18 @@ answerOne.on("click", function() {
     i++;
     j += 2;
     k += 2;
-    if (i == 4) {
+} else if (i == 3) {
+        text += keywords[j] + " ";
+        $('#results').val(text);
         answerOne.fadeOut(1000);
         answerTwo.fadeOut(1000);
+        $('#quiz-button').delay(1500).fadeIn(500);
         $('#quiz-button').prop("type", "submit");
     }
     console.log(text);
 });
 answerTwo.on("click", function() {
+    if (i < 3) {
     question.fadeOut(1000, function() {
         question.text(questionsToAsk[i]).fadeIn(1000);
     });
@@ -50,9 +55,12 @@ answerTwo.on("click", function() {
     i++;
     j += 2;
     k += 2;
-    if (i == 4) {
+} else if (i == 3) {
+        text += keywords[k] + " ";
+        $('#results').val(text);
         answerOne.fadeOut(1000);
         answerTwo.fadeOut(1000);
+        $('#quiz-button').delay(1500).fadeIn(500);
         $('#quiz-button').prop("type", "submit");
     }
     console.log(text);
