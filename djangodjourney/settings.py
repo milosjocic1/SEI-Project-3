b0c9711414070e73ed65f2c86383f066235f46e6
alex-sasha-ward <amwtranslations@gmail.com>
2022-10-06 15:01:08 +0100

    Changes

--- djangodjourney/settings.py
+++ djangodjourney/settings.py
@@ -80,10 +80,10 @@ WSGI_APPLICATION = 'djangodjourney.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'PORT': ''
+        'NAME': os.getenv('DATABASENAME'),
+        'USER': os.getenv('DATABASEUSER'),
+        'PASSWORD': os.getenv('DATABASEPASSWORD'),
+        'PORT': os.getenv('PORT')
     }
 }
 
@@ -131,6 +131,3 @@ LOGOUT_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-import django_heroku
-django_heroku.settings(locals())
